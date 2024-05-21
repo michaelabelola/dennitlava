fb31a80fcb0e660c945d45a023881cb289cbf908
Abel Michael <michaelabelola@gmail.com>
2024-05-22 00:30:55 +0100

    first commit

---
+++ src/main/java/com/dennitlava/dennitlava/DennitlavaApplication.java
@@ -0,0 +1,13 @@
+package com.dennitlava.dennitlava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DennitlavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DennitlavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dennitlava/dennitlava/DennitlavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dennitlava.dennitlava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DennitlavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
